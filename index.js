aa00d00ac7941b0ca2ec7a3490186ec1ee92cc1e
Ashley Hayes <ashleymh060504@gmail.com>
2024-10-30 14:48:59 -0400

    required pkgs, added questions, created f to write README.

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 // TODO: Include packages needed for this application
+const inquirer = require(`inquirer`)
+const fs = require(`fs`)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    `What is your project title?`, 
+    `Provide a description of your project:`, 
+    `What are your installation instructions?`, 
+    `Provide your usage information`, 
+    `What are your contribution guidelines?`, 
+    `Please provide your test instructions:`
+    `Choose a license for your project`,
+    `What is your GitHub username?`,
+    `What is your email address?`,
+ ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(`Error writing to file:`, err);
+            return;
+        }
+        console.log(`README file has been created successfully!`);
+    });
+}
+//To call this function:
+// const readmeContent = `# Project Title\n\nDescription of the project...`;
+// writeToFile('README.md', readmeContent);
 
 // TODO: Create a function to initialize app
 function init() {}
