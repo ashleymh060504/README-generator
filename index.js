e7ee744a5553b4a164dea3ebd4f4f8cf81085447
Ashley Hayes <ashleymh060504@gmail.com>
2024-10-30 22:04:18 -0400

    corrected questions array syntax, worked on function to write README file

--- index.js
+++ index.js
@@ -1,33 +1,76 @@
 // TODO: Include packages needed for this application
 const inquirer = require(`inquirer`)
-const fs = require(`fs`)
+const fs = require(`fs`);
+const { default: generateMarkdown } = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    `What is your project title?`, 
-    `Provide a description of your project:`, 
-    `What are your installation instructions?`, 
-    `Provide your usage information`, 
-    `What are your contribution guidelines?`, 
-    `Please provide your test instructions:`
-    `Choose a license for your project`,
-    `What is your GitHub username?`,
-    `What is your email address?`,
+    {
+        type: `input`,
+        name: `title`,
+        message: `What is your project title?`,
+    },
+    {
+        type: `input`,
+        name: `description`,
+        message: `Provide a description of your project:`,
+    },
+    {
+        type: `input`,
+        name: `installation`,
+        message: `What are your installation instructions?`,
+    },
+    {
+        type: `input`,
+        name: `usage`,
+        message: `Provide your usage information`,
+    },
+    {
+        type: `input`,
+        name: `contribution`,
+        message: `What are your contribution guidelines?`,
+    },
+    {
+        type: `input`,
+        name: `test`,
+        message: `Please provide your test instructions:`,
+    },
+    {
+        type: `list`,
+        name: `license`,
+        message: `Choose a license for your project`,
+        choices: [
+            `MIT`,
+            `Apache License 2.0`,
+            `GNU General Public License (GPL) v3`,
+            `BSD 3-Clause License`,
+        ],
+    },
+    {
+        type: `input`,
+        name: `github`,
+        message: `What is your GitHub username?`,
+    },
+    {
+        type: `input`,
+        name: `email`,
+        message: `What is your email address?`,
+    },
  ];
+ // TODO: Create a function to write README file
+const readmeContent = generateMarkdown(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {
-            console.error(`Error writing to file:`, err);
-            return;
-        }
-        console.log(`README file has been created successfully!`);
-    });
-}
-//To call this function:
-// const readmeContent = `# Project Title\n\nDescription of the project...`;
-// writeToFile('README.md', readmeContent);
+ inquirer.prompt(questions).then((answers) => {
+    console.log(`Your answers:`, answers);
+    fs.writeFile(`README.md`, readmeContent, (err) => {
+            if (err) {
+                console.error(`Error writing to file:`, err);
+                return;
+            }
+            console.log(`README file has been created successfully!`);
+        });
+    }
+)
 
 // TODO: Create a function to initialize app
 function init() {}
